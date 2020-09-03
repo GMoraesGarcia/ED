32d3c69169b4d9bf5a59106b88defc804db358e2
GMoraesGarcia <g.moraes.garcia@gmail.com>
2020-09-03 12:28:46 -0300

    Mudança no método de leVetor para leVetorAleatorio

--- ED_3NA/ED_3NA/src/Aula03_desempenho/EX_08_Intercalacao.java
+++ ED_3NA/ED_3NA/src/Aula03_desempenho/EX_08_Intercalacao.java
@@ -8,6 +8,7 @@ import Aula01_Vetores.EX_04_Inverter;
 import static Aula01_Vetores.EX_04_Inverter.exibeVetor;
 import static Aula01_Vetores.EX_04_Inverter.leInteiroPositivo;
 import static Aula01_Vetores.EX_04_Inverter.leVetor;
+import static Aula01_Vetores.EX_04_Inverter.leVetorAleatorio;
 /**
  *
  * @author Gabriel
@@ -17,8 +18,8 @@ public class EX_08_Intercalacao {
         int n  = leInteiroPositivo();
         int m  = leInteiroPositivo();
         
-        int[] v1 = leVetor(n);
-        int[] v2 = leVetor(m);
+        int[] v1 = leVetorAleatorio(n);
+        int[] v2 = leVetorAleatorio(m);
          exibeVetor(v1);
          exibeVetor(v2);
         exibeVetor(intercala(v1, v2));
@@ -26,7 +27,7 @@ public class EX_08_Intercalacao {
     
     public static int[] intercala(int[] v1,int[] v2){
          int v3[] = new int[v1.length+v2.length];
-        int k=0,i=0,j=0,count =0;
+        int k=0,i=0,j=0,count =0;//count contador de passos
         while(i<v1.length && j<v2.length){
             if(v1[i]< v2[j]){
                 v3[k++] = v1[i++];
